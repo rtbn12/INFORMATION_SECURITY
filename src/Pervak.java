e29a92e7de9c190f8994cb692d7e96733ed3a5d4
Egor Solodov <solodov_2007@bk.ru>
2026-03-09 01:43:01 +0300

    1commit

--- src/Pervak.java
+++ src/Pervak.java
@@ -22,6 +22,7 @@ public class Pervak {
                     "0 - Выход из программы\n" +
                     "1 - Алгоритм Виженера\n" +
                     "2 - Алгоритм Цезаря\n" +
+                    "3 - Алгоритм RSA\n" +
                     "Ваш вариант:");
 
 
@@ -39,6 +40,9 @@ public class Pervak {
                         break;
                     case 0:
                         algoritmCycle = false;
+                        break;
+                    case 3:
+                        RSA(scanner);
                         break;
                     default:
                         System.out.println("Такого варианта выбора нет!\n" +
@@ -790,6 +794,343 @@ public class Pervak {
 
     }
 
+    public static void RSA(Scanner scanner){
+
+        int printChoice;
+        boolean print = true;
+        while(print)
+        {
+            System.out.println("=============Алгоритм RSA==========");
+            System.out.print("Выберите режим работы:\n" +
+                    "0 - Выход из программы\n" +
+                    "1 - Шифрование\n" +
+                    "2 - Дешифрование\n" +
+                    "Ваш вариант:");
+
+
+            try {
+                printChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (printChoice)
+                {
+                    case 1:
+                        shifrovanieRSA(scanner);
+                        break;
+                    case 2:
+                        deshifrovanieRSA(scanner);
+                        break;
+                    case 0:
+                        print = false;
+                        break;
+                    default:
+                        System.out.println("Такого варианта выбора нет!\n" +
+                                "Пожалуйста, введите корректное число!");
+                }
+
+            }catch (InputMismatchException e) {
+                System.out.println("Произошла ошибка!\n" +
+                        "Пожалуйста, введите корректное целочисленное значение выбранного варианта!\n" +
+                        "В прошлый раз вы ввели букву вместо числа!");
+                scanner.nextLine();
+            }
+            catch (Exception a) {
+                System.out.println("Произошла неизвестная ошибка!");
+                scanner.nextLine();
+            }
+
+        }
 
     }
+
+    public static void shifrovanieRSA(Scanner scanner){
+        String name_ishodnik = "";
+        String name_end = "";
+
+
+        HashMap<Integer, Character> dictionary = createDictionary();
+
+
+        HashMap<Character, Integer> reverseDict = new HashMap<>();
+        for (Map.Entry<Integer, Character> entry : dictionary.entrySet()) {
+            reverseDict.put(entry.getValue(), entry.getKey());
+        }
+
+        int dictSize = dictionary.size();
+
+        boolean cycle = true;
+        while (cycle) {
+            System.out.print("Введите название файла, который хотите зашифровать (не забудьте добавить .txt)\n" +
+                    "Название: ");
+
+            name_ishodnik = scanner.nextLine();
+
+            if (name_ishodnik.trim().isEmpty()) {
+                System.out.println("Имя файла не может быть пустым!");
+                continue;
+            }
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(name_ishodnik))) {
+                ArrayList<String> lines = new ArrayList<>();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    lines.add(line);
+                }
+
+                cycle = false;
+
+                System.out.print("Введите название файла для зашифрованного результата (с .txt): ");
+                name_end = scanner.nextLine();
+
+                if (name_end.trim().isEmpty()) {
+                    name_end = name_ishodnik.replace(".txt", "_encrypted.txt");
+                    if (name_end.equals(name_ishodnik)) {
+                        name_end = name_ishodnik + "_encrypted.txt";
+                    }
+                    System.out.println("Будет использовано имя: " + name_end);
+                }
+
+                int[] keys = KeysRsaGenerator(dictSize);
+                int n = keys[2];
+                int e = keys[3];
+                int d = keys[4];
+
+                try (PrintWriter writer = new PrintWriter(name_end)) {
+                    int isxodIndex;
+                    int newIndex;
+                    for (String originalLine : lines) {
+                        StringBuilder encryptedLine = new StringBuilder();
+
+                        for (char c : originalLine.toCharArray()) {
+                            if(reverseDict.containsKey(c)){
+                                isxodIndex = reverseDict.get(c);
+                                newIndex = (int) modPow(isxodIndex, e,n);
+
+                                encryptedLine.append(newIndex).append(" ");
+
+
+                            }else{
+
+                                encryptedLine.append(c);
+                            }
+                        }
+
+                        writer.println(encryptedLine.toString());
+                    }
+
+                    System.out.println("Файл успешно зашифрован и сохранен: " + name_end);
+                    System.out.println("Ключи : n = " + n + " e = " + e + " d = " + d);
+
+
+
+                } catch (FileNotFoundException u) {
+                    System.out.println("Не удалось создать выходной файл: " + u.getMessage());
+                }
+
+
+
+                String keyFileName = name_end + "_keys.txt";
+                try (PrintWriter writer_2 = new PrintWriter(keyFileName)) {
+
+                    writer_2.println("Ключи : n = " + n + " e = " + e + " d = " + d);
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("\nФайл с таким именем не найден, попробуйте ввести имя ещё раз!");
+            } catch (IOException e) {
+                System.out.println("Ошибка чтения файла: " + e.getMessage());
+                System.out.println("Попробуйте ещё раз!");
+            }
+        }
+
+    }
+
+    static void deshifrovanieRSA(Scanner scanner) {
+        String name_ishodnik = "";
+        String name_end = "";
+
+        HashMap<Integer, Character> dictionary = createDictionary();
+
+        boolean cycle = true;
+        while (cycle) {
+            System.out.print("Введите название зашифрованного файла (с .txt): ");
+            name_ishodnik = scanner.nextLine();
+
+            if (name_ishodnik.trim().isEmpty()) {
+                System.out.println("Имя файла не может быть пустым!");
+                continue;
+            }
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(name_ishodnik))) {
+
+
+                System.out.print("Введите закрытый ключ d: ");
+                int d = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Введите модуль n: ");
+                int n = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.print("Введите название файла для расшифрованного результата (с .txt): ");
+                name_end = scanner.nextLine();
+
+                if (name_end.trim().isEmpty()) {
+                    name_end = name_ishodnik.replace(".txt", "_decrypted.txt");
+                    System.out.println("Будет использовано имя: " + name_end);
+                }
+
+                try (PrintWriter writer = new PrintWriter(name_end)) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+
+                        String[] parts = line.split(" ");
+                        StringBuilder decryptedLine = new StringBuilder();
+
+                        for (String part : parts) {
+                            if (part.isEmpty()) continue;
+
+
+                            boolean isNumber = true;
+                            for (int i = 0; i < part.length(); i++) {
+                                if (!Character.isDigit(part.charAt(i))) {
+                                    isNumber = false;
+                                    break;
+                                }
+                            }
+
+                            if (isNumber) {
+
+                                long encrypted = Long.parseLong(part);
+                                long m = modPow(encrypted, d, n);
+
+
+                                Character originalChar = dictionary.get((int) m);
+                                if (originalChar != null) {
+                                    decryptedLine.append(originalChar);
+                                } else {
+                                    decryptedLine.append('?');
+                                }
+                            } else {
+
+                                decryptedLine.append(part);
+                            }
+                        }
+                        writer.println(decryptedLine.toString());
+                    }
+
+                    System.out.println("Файл успешно расшифрован и сохранен: " + name_end);
+                    System.out.println("Использованы Ключи : n = " + n +  " d = " + d);
+                    cycle = false;
+                }
+
+            } catch (FileNotFoundException e) {
+                System.out.println("\nФайл с таким именем не найден, попробуйте ввести имя ещё раз!");
+            } catch (IOException e) {
+                System.out.println("Ошибка чтения файла: " + e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка: нужно вводить целые числа!");
+                scanner.nextLine();
+            }
+        }
+    }
+
+
+    public static int[] KeysRsaGenerator(int size){
+
+        Random random = new Random();
+        int[] primeNumbers = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
+                73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,
+                179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,
+                283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,
+                419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,
+                547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,
+                661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,
+                811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,
+                947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,
+                1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223,
+                1229,1231,1237,1249,1259,1277,1279,1283,1289,1291,1297,1301,1303,1307,1319,1321,1327,1361,1367,1373,
+                1381,1399,1409,1423,1427,1429,1433,1439,1447,1451,1453,1459,1471,1481,1483,1487,1489,1493,1499,1511,
+                1523,1531,1543,1549,1553,1559,1567,1571,1579,1583,1597,1601,1607,1609,1613,1619,1621,1627,1637,1657,
+                1663,1667,1669,1693,1697,1699,1709,1721,1723,1733,1741,1747,1753,1759,1777,1783,1787,1789,1801,1811,
+                1823,1831,1847,1861,1867,1871,1873,1877,1879,1889,1901,1907,1913,1931,1933,1949,1951,1973,1979,1987,
+                1993,1997,1999,2003,2011,2017,2027,2029,2039,2053,2063,2069,2081,2083,2087,2089,2099,2111,2113,2129,
+                2131,2137,2141,2143,2153,2161,2179,2203,2207,2213,2221,2237,2239,2243,2251,2267,2269,2273,2281,2287,
+                2293,2297,2309,2311,2333,2339,2341,2347,2351,2357,2371,2377,2381,2383,2389,2393,2399,2411,2417,2423,
+                2437,2441,2447,2459,2467,2473,2477,2503,2521,2531,2539,2543,2549,2551,2557,2579,2591,2593,2609,2617,
+                2621,2633,2647,2657,2659,2663,2671,2677,2683,2687,2689,2693,2699,2707,2711,2713,2719,2729,2731,2741,
+                2749,2753,2767,2777,2789,2791,2797,2801,2803,2819,2833,2837,2843,2851,2857,2861,2879,2887,2897,2903,
+                2909,2917,2927,2939,2953,2957,2963,2969,2971,2999,3001,3011,3019,3023,3037,3041,3049,3061,3067,3079,
+                3083,3089,3109,3119,3121,3137,3163,3167,3169,3181,3187,3191,3203,3209,3217,3221,3229,3251,3253,3257,
+                3259,3271,3299,3301,3307,3313,3319,3323,3329,3331,3343,3347,3359,3361,3371,3373,3389,3391,3407,3413,
+                3433,3449,3457,3461,3463,3467,3469,3491,3499,3511,3517,3527,3529,3533,3539,3541,3547,3557,3559,3571};
+
+        int primaryNumbersLength = primeNumbers.length;
+
+        int p =0;
+        int q =0;
+        int n =0;
+        int e = 0;
+        int d = 1;
+
+        boolean cycle = false;
+        while (!cycle){
+            while (n<=size){
+                p = primeNumbers[random.nextInt(primaryNumbersLength)];
+                q = primeNumbers[random.nextInt(primaryNumbersLength)];
+                n= p*q;
+            }
+
+            while (e <= 1 || e >= (p-1)*(q-1) || gcd(e, (p-1)*(q-1)) != 1) {
+                e = primeNumbers[random.nextInt(primaryNumbersLength)];
+            }
+
+            while (((e*d)%((q-1)*(p-1)))!=1){
+                d++;
+                if(d >= (q-1)*(p-1)){
+                    System.out.println("Не удалось подобрать число d!");
+                    System.out.println("Попытаюсь попробовать снова!");
+                    break;
+                }
+                else{
+                    cycle= true;
+                }
+
+            }
+        }
+
+        int[] keys = {p,q,n,e,d};
+        return keys;
+    }
+    //  метод для НОД
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+
+
+
+
+    }
+    // чисто математический метод для возведения в степень и нахождения остатка от деления
+    public static long modPow(long base, long exponent, long modulus) {
+        long result = 1;                 // (1) Начинаем с 1
+        base = base % modulus;            // (2) Приводим базу к диапазону модуля
+
+        while (exponent > 0) {            // (3) Пока есть степень
+            if (exponent % 2 == 1) {      // (4) Если текущий бит степени = 1
+                result = (result * base) % modulus; // (5) Умножаем результат
+            }
+            base = (base * base) % modulus; // (6) Квадрат базы
+            exponent = exponent >> 1;      // (7) Делим степень на 2 (сдвиг вправо)
+        }
+
+        return result;                    // (8) Возвращаем результат
+    }
+}
+
 
